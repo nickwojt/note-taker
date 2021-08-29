e7be565c6ea9b35e5c4930e42cc9fc14aedce034
Nicholas Wojtalewicz <nicholaswojt@outlook.com>
2021-08-29 11:29:40 -0600

    Added route handlers and tested

--- server.js
+++ server.js
@@ -1,29 +1,66 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const fs = require("fs");
-const myModule = "./myModule";
+const { randomUUID } = require("crypto");
+const util = require("util");
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get("/trees", (req, res) => {
-  res.status(200).send({
-    tree: "🌳",
-    type: "Oak",
+const writeToFile = util.promisify(fs.writeFile);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.send(data);
   });
 });
 
-app.post("/trees/:id", (req, res) => {
-  const id = req.params.id;
-  const { type } = req.body;
-
-  if (req.body) {
-    res.status(201).send({
-      tree: `🌳 is this type: ${type} and has an ID of ${id}`,
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  let newNote = {};
+  if (title && text) {
+    newNote = {
+      title,
+      text,
+      id: randomUUID(),
+    };
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      const allNotes = [].concat(JSON.parse(data));
+      allNotes.push(newNote);
+      console.log(typeof data);
+      console.log(allNotes);
+      writeToFile("./db/db.json", JSON.stringify(allNotes), (err) => {
+        err ? console.log(err) : console.log("Success. File was written!");
+      })
+        .then(
+          res.status(201).sendFile(path.join(__dirname, "./public/notes.html"))
+        )
+        .catch(err ? console.log(err) : console.log("Woo!"));
     });
-  } else {
-    res.status(418).send({ message: "We need a logo!" });
   }
 });
 
+// app.delete("/api/notes/:id", (req, res) => {
+//   const id = req.params.id;
+//   if id
+// });
+
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+
+//let arr = [1, 3, 5, 4]
+// let newArr = [...arr, 5] <== [1, 3, 5, 4, 5]
